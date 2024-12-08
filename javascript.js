a417c6162de270580c3093fb7dfffa6924dcde5f
AliceAndree <thiel.alice@gmail.com>
2024-12-08 18:48:25 +0100

    creation of basic operation functions
    - add()
    - substract()
    - multiply()
    - divide()

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function add(numberOne, numberTwo) {
+  const sum = numberOne + numberTwo;
+  return sum;
+}
+
+function substract(numberOne, numberTwo) {
+  const substraction = numberOne - numberTwo;
+  return substraction;
+}
+
+function multiply(numberOne, numberTwo) {
+  const product = numberOne * numberTwo;
+  return product;
+}
+
+function divide(numberOne, numberTwo) {
+  const division = numberOne / numberTwo;
+  return division;
+}
+
+console.log(add(3, 5));
+console.log(substract(3, 5));
+console.log(multiply(3, 5));
+console.log(divide(3, 5));
