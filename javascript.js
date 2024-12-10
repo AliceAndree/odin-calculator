f43b802d6f257a50d3676ee0b9fb519081619616
AliceAndree <thiel.alice@gmail.com>
2024-12-10 12:18:54 +0100

    first version of calculator to be refactored

--- javascript.js
+++ javascript.js
@@ -1,28 +1,29 @@
 const screen = document.querySelector("#screen");
 const calculatorButtons = document.querySelectorAll(".calculator");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector("#clear");
 const calculationDisplay = document.createElement("span");
+const resultDisplay = document.createElement("span");
 const operationArray = [];
 
 calculationDisplay.setAttribute("id", "operation");
+resultDisplay.setAttribute("id", "result");
 
 function add(numberOne, numberTwo) {
-  const sum = numberOne + numberTwo;
-  return sum;
+  const result = numberOne + numberTwo;
+  displayResult(result);
 }
-
 function substract(numberOne, numberTwo) {
-  const substraction = numberOne - numberTwo;
-  return substraction;
+  const result = numberOne - numberTwo;
+  displayResult(result);
 }
-
 function multiply(numberOne, numberTwo) {
-  const product = numberOne * numberTwo;
-  return product;
+  const result = numberOne * numberTwo;
+  displayResult(result);
 }
-
 function divide(numberOne, numberTwo) {
-  const division = numberOne / numberTwo;
-  return division;
+  const result = numberOne / numberTwo;
+  displayResult(result);
 }
 
 function operate(operator, numberOne, numberTwo) {
@@ -48,8 +49,57 @@ function displayOperation(operation) {
   return concatArray;
 }
 
+function readOperation() {
+  const concatArray = displayOperation();
+  const operationArray = concatArray
+    .split("+")
+    .join(",")
+    .split("-")
+    .join(",")
+    .split("*")
+    .join(",")
+    .split("/")
+    .join(",")
+    .split(",");
+
+  const operatorArray = concatArray
+    .split("1")
+    .join("")
+    .split("2")
+    .join("")
+    .split("3")
+    .join("")
+    .split("4")
+    .join("")
+    .split("5")
+    .join("")
+    .split("6")
+    .join("")
+    .split("7")
+    .join("")
+    .split("8")
+    .join("")
+    .split("9")
+    .join("")
+    .split(",");
+
+  numberOne = parseInt(operationArray[0]);
+  numberTwo = parseInt(operationArray[1]);
+  operator = operatorArray[0];
+
+  operate(operator, numberOne, numberTwo);
+}
+
+function displayResult(result) {
+  calculationDisplay.remove();
+  resultDisplay.textContent = result;
+  screen.appendChild(resultDisplay);
+}
+
 calculatorButtons.forEach((element) => {
   element.addEventListener("click", function (e) {
     displayOperation(e.target.value);
   });
 });
+
+equalButton.addEventListener("click", readOperation);
